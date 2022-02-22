231ca98c814f8ee62c1de79efac9466b12f79982
ZinoTrust <zinotrust@gmail.com>
2022-02-22 14:05:21 +0100

    Linking state to form inputs

--- src/components/taskManager/TaskManager.js
+++ src/components/taskManager/TaskManager.js
@@ -1,21 +1,43 @@
-import React from "react";
+import { useState } from "react";
 import Task from "./Task";
 import "./TaskManager.css";
 
 const TaskManager = () => {
+  const [name, setName] = useState("");
+  const [date, setDate] = useState("");
+  const [tasks, setTasks] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(name);
+    console.log(date);
+  };
+
   return (
     <div className="--bg-primary">
       <h1 className="--text-center --text-light">Task Manager</h1>
       <div className="--flex-center --p">
         <div className="--card --bg-light --width-500px --p --flex-center">
-          <form className="form --form-control">
+          <form onSubmit={handleSubmit} className="form --form-control">
             <div>
               <label htmlFor="name">Task:</label>
-              <input type="text" placeholder="Task name" name="name" />
+              <input
+                type="text"
+                placeholder="Task name"
+                name="name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </div>
             <div>
               <label htmlFor="date">Task:</label>
-              <input type="date" placeholder="Task name" name="date" />
+              <input
+                type="date"
+                placeholder="Task name"
+                name="date"
+                value={date}
+                onChange={(e) => setDate(e.target.value)}
+              />
             </div>
             <button className="--btn --btn-success --btn-block">
               Save Task
