9e719b5292f825849882ac151eb952e5990f8907
ZinoTrust <zinotrust@gmail.com>
2022-02-22 13:00:36 +0100

    Task manager jsx

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import "./App.scss";
+import TaskManager from "./components/taskManager/TaskManager";
 
 function App() {
   return (
     <div>
-      <h1>Hello World!!!</h1>
+      <TaskManager />
     </div>
   );
 }
---
+++ src/components/taskManager/TaskManager.js
@@ -0,0 +1,29 @@
+import React from "react";
+import "./TaskManager.css";
+
+const TaskManager = () => {
+  return (
+    <div className="--bg-primary">
+      <h1 className="--text-center --text-light">Task Manager</h1>
+      <div className="--flex-center --p">
+        <div className="--card --bg-light --width-500px --p --flex-center">
+          <form className="form --form-control">
+            <div>
+              <label htmlFor="name">Task:</label>
+              <input type="text" placeholder="Task name" name="name" />
+            </div>
+            <div>
+              <label htmlFor="date">Task:</label>
+              <input type="date" placeholder="Task name" name="date" />
+            </div>
+            <button className="--btn --btn-success --btn-block">
+              Save Task
+            </button>
+          </form>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TaskManager;
