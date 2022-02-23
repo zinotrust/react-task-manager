9ab4582a90088352ac06182a28cfd10d9f4239ae
ZinoTrust <zinotrust@gmail.com>
2022-02-23 17:47:06 +0100

    useReducer setup

--- src/components/taskManager/TaskManagerReducer.js
+++ src/components/taskManager/TaskManagerReducer.js
@@ -1,19 +1,27 @@
-import { useState, useRef, useEffect } from "react";
+import { useState, useRef, useEffect, useReducer } from "react";
 import useLocalStorage from "use-local-storage";
 import Alert from "../alert/Alert";
 import Confirm from "../confirm/Confirm";
 import Task from "./Task";
 import "./TaskManager.css";
 
+const taskReducer = (state, action) => {};
+
 const TaskManagerReducer = () => {
   const [name, setName] = useState("");
   const [date, setDate] = useState("");
-  // const [tasks, setTasks] = useState([]);
-
   const [tasks, setTasks] = useLocalStorage("tasks", []);
 
-  const [taskID, setTaskID] = useState(null);
-  const [isEditing, setIsEditing] = useState(false);
+  const initialState = {
+    tasks,
+    taskID: null,
+    isEditing: false,
+    isAlertOpen: true,
+    alertContent: "This is an alert",
+    alertClass: "success",
+  };
+
+  const [state, dispatch] = useReducer(taskReducer, initialState);
 
   const nameInputRef = useRef(null);
 
@@ -23,64 +31,19 @@ const TaskManagerReducer = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if ((!name && !date) || !name || !date) {
-      alert("Please enter task name and date");
-    } else if (name && date && isEditing) {
-      setTasks(
-        tasks.map((task) => {
-          if (task.id === taskID) {
-            return { ...task, name, date, complete: false };
-          }
-          return task;
-        })
-      );
-      setName("");
-      setDate("");
-      setIsEditing(false);
-      setTaskID(null);
-    } else {
-      const newTask = {
-        id: Date.now(),
-        name,
-        date,
-        complete: false,
-      };
-      setTasks([...tasks, newTask]);
-      setName("");
-      setDate("");
-    }
   };
 
-  const editTask = (id) => {
-    const thisTask = tasks.find((task) => task.id === id);
-    setIsEditing(true);
-    setTaskID(id);
-    setName(thisTask.name);
-    setDate(thisTask.date);
-  };
+  const editTask = (id) => {};
 
-  const deleteTask = (id) => {
-    if (window.confirm("Delete this task") === true) {
-      const newTasks = tasks.filter((task) => task.id !== id);
-      setTasks(newTasks);
-    }
-  };
+  const deleteTask = (id) => {};
 
-  const completeTask = (id) => {
-    setTasks(
-      tasks.map((task) => {
-        if (task.id === id) {
-          return { ...task, complete: true };
-        }
-        return task;
-      })
-    );
-  };
+  const completeTask = (id) => {};
 
   return (
     <div className="--bg-primary">
-      {/* <Alert /> */}
-      <Confirm />
+      {state.isAlertOpen && <Alert />}
+
+      {/* <Confirm /> */}
       <h2 className="--text-center --text-light">Task Manager Reducer</h2>
       <div className="--flex-center --p">
         <div className="--card --bg-light --width-500px --p --flex-center">
